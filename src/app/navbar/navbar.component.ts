3452fd00fdc673fdc6a85e31e4bbc4f0122b4e25
OleksandrTkachenko <ha100790tag@gmail.com>
2018-02-15 13:40:12 +0200

    some fixes for register/login pages

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs/Observable";
 import { AuthService } from "../services/auth.service";
 import * as firebase from 'firebase/app';
+import {select} from '@angular-redux/store';
+import {IAppState} from '../store';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,9 @@ import * as firebase from 'firebase/app';
 })
 export class NavbarComponent implements OnInit {
   user: Observable<firebase.User>;
+  @select((s:IAppState) => s.user) user$;
+  logo: string = "assets/image/beta-logo.png";
+  logoRoute: string = "/login";
   userEmail: string;
 
   constructor(private authService: AuthService) { }
@@ -19,11 +24,13 @@ export class NavbarComponent implements OnInit {
     this.user.subscribe(user => {
       if (user) {
         this.userEmail = user.email;
+        this.logoRoute = "/books";
       }
     });
   }
 
   logout() {
     this.authService.logout();
+    this.logoRoute = "/login"
   }
 }
